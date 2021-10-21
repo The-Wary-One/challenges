03a03c87f5e0b13aa6ad095667fd282ce4a43713
Wary <one.should.be.wary@gmail.com>
2021-10-21 11:09:35 +0200

    ✨ withdraw function

--- packages/hardhat/contracts/DiamondHands.sol
+++ packages/hardhat/contracts/DiamondHands.sol
@@ -20,13 +20,27 @@ contract DiamondHands {
         return _hands[_addr].lockedTil;
     }
 
+    function hasDeposited(address _addr) public view returns (bool) {
+        return balanceOf(_addr) > 0;
+    }
+
+    function canWithdraw(address _addr) public view returns (bool) {
+        return block.timestamp >= lockedUntil(_addr) && hasDeposited(_addr);
+    }
+
     function deposit() external payable {
-        require(msg.value > 0);
+        require(msg.value > 0, 'Insufficient funds');
         _hands[msg.sender].balance += msg.value;
         _hands[msg.sender].lockedTil = block.timestamp + lockPeriod;
     }
 
     function withdraw() external {
-
+        // Check
+        require(canWithdraw(msg.sender), 'Cannot withdraw');
+        // Effects
+        uint256 _balance = balanceOf(msg.sender);
+        delete _hands[msg.sender];
+        // Interactions
+        payable(msg.sender).transfer(_balance);
     }
 }
--- packages/hardhat/test/diamondHands.test.ts
+++ packages/hardhat/test/diamondHands.test.ts
@@ -21,7 +21,7 @@ describe('DiamondHands', () => {
     describe('deposit', () => {
         it('should revert if 0 ETH was send', async () => {
             const { diamondHands } = await setupTests()
-            await expect(diamondHands.deposit({ value: '0x00' })).to.be.reverted
+            await expect(diamondHands.deposit({ value: 0 })).to.be.revertedWith('Insufficient funds')
         })
 
         it("should add the received amount to the user's balance and reset the locking period", async () => {
@@ -45,6 +45,31 @@ describe('DiamondHands', () => {
             block = await hre.ethers.provider.getBlock(receipt.blockNumber)
             expect(await diamondHands.balanceOf(user1.address)).to.equal(oneEther.mul(2))
             expect(await diamondHands.lockedUntil(user1.address)).to.equal(block.timestamp + TWO_YEARS_IN_SECONDS)
+        })
+    })
+
+    describe('withdraw', () => {
+        it('should revert when the user never deposited', async () => {
+            const { diamondHands } = await setupTests()
+            await expect(diamondHands.withdraw()).to.be.revertedWith('Cannot withdraw')
+        })
+
+        it('should revert when the funds are still locked', async () => {
+            const { hre, diamondHands } = await setupTests()
+            const oneEther = hre.ethers.constants.WeiPerEther
+            await diamondHands.deposit({ value: oneEther })
+            await expect(diamondHands.withdraw()).to.be.revertedWith('Cannot withdraw')
+            await hre.timeAndMine.setTimeIncrease(`${TWO_YEARS_IN_SECONDS - 2}`)
+            await expect(diamondHands.withdraw()).to.be.revertedWith('Cannot withdraw')
+        })
+
+        it('should withdraw once the funds after the locking period', async () => {
+            const { hre, user1, diamondHands } = await setupTests()
+            const oneEther = hre.ethers.constants.WeiPerEther
+            await diamondHands.deposit({ value: oneEther })
+            await hre.timeAndMine.setTimeIncrease(TWO_YEARS_IN_SECONDS.toString())
+            await expect(await diamondHands.withdraw()).to.changeEtherBalance(user1, oneEther)
+            await expect(diamondHands.withdraw()).to.be.revertedWith('Cannot withdraw')
         })
     })
 })
